be8d3c136c21916aff410a361ce51b3d37945c8c
Atta Adou <aadou021@uottawa.ca>
2026-01-31 00:27:40 -0500

    Classe Resident et Program terminé et en bonne etat de fonctionnement- Documention manquante

--- Program.java
+++ Program.java
@@ -4,115 +4,154 @@
 
 // this is the (incomplete) Program class
 public class Program {
-	
-	private String programID;
-	private String name;
-	private int quota;
-	private int[] rol;
-	private Resident[] matchedResidents;
-	
-	private int matchedCount=0;
-	
-	 // index du prochain résident à proposer
-	
-	// constructs a Program
-    public Program(String id, String n, int q) {
-	
-		programID= id;
-		name= n;
-		quota= q;
-		matchedResidents= new Resident[quota];
-		
 
-		
-	}
+    private String programID;
+    private String name;
+    private int quota;
+    private int[] rol;
+    private Resident[] matchedResidents;
+
+    private int matchedCount = 0;
+
+    // index du prochain résident à proposer
+    // constructs a Program
+    public Program(String id, String n, int q) {
+
+        programID = id;
+        name = n;
+        quota = q;
+        matchedResidents = new Resident[quota];
+
+    }
 
     // the rol in order of preference
-	public void setROL(int[] rol) {
-		
-		this.rol= rol;
-	}
+    public void setROL(int[] rol) {
 
-	public int[] getrol(){
-		return rol;
-	}
-	
-	// string representation
-	public String toString() {
-      
-       return "["+programID+"]: "+name+" {"+ quota+ "}" +" ("+rol.length+")";	  
-	}
+        this.rol = rol;
+    }
 
-	//Getters et setters
+    public int[] getrol() {
+        return rol;
+    }
 
-	public void setID(String ID){
-		this.programID = ID;
-	}
+    // string representation
+    public String toString() {
 
-	public String getID(){
-		return programID;
-	}
+        return "[" + programID + "]: " + name + " {" + quota + "}" + " (" + rol.length + ")";
+    }
 
-	public void setName(String name){
-		this.name = name;
-	}
+    //Getters et setters
+    public void setID(String ID) {
+        this.programID = ID;
+    }
 
-	public String getName(){
-		return name;
-	}
+    public String getID() {
+        return programID;
+    }
 
-	public void setQuota(int quota){
-		this.quota = quota;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public int getQuota(){
-		return quota;
-	}
-	
+    public String getName() {
+        return name;
+    }
 
-	//Nouvelles méthodes 
+    public void setQuota(int quota) {
+        this.quota = quota;
+    }
 
+    public int getQuota() {
+        return quota;
+    }
 
-	public boolean member (int residentID){ // pas faite - retourne vrai si l'utilisateur fait parti de la liste de preference pour ce programme
-		for (int i = 0; i < rol.length; i++){ 
-			if (rol[i] == residentID){
-				return true;
-			}
-		}
-		return false;
-	}
+    //Nouvelles méthodes 
+    public boolean member(int residentID) { // pas faite - retourne vrai si l'utilisateur fait parti de la liste de preference pour ce programme
+        for (int i = 0; i < rol.length; i++) {
+            if (rol[i] == residentID) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public int rank (int residentID){ // retourne le rang du resident dans le rol du programme
-		for (int i = 0; i < rol.length; i++){
-			if (rol[i] == residentID){
-				return i;
-			}
-		}
-		return -1;
-	}
+    public int rank(int residentID) { // retourne le rang du resident dans le rol du programme
+        for (int i = 0; i < rol.length; i++) {
+            if (rol[i] == residentID) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	public Resident leastPreferred(){
-		if (matchedCount == 0){
-			return null ; // pas de resident
-		}
-		Resident leastPreferredResident = matchedResidents[0];
-		int leastPreferredRank = rank(leastPreferredResident.getId());
-		for (int i = 0; i < matchedCount; i++){
-			Resident residentID = matchedResidents[i]; // convertir en int
-			int rank = rank(residentID.getId());
-			if (rank > leastPreferredRank){ // on veut le rang le plus élevé 
-				leastPreferredRank = rank;
-				leastPreferredResident = residentID;
-			}
-		}
-		return leastPreferredResident;
-	}
+    public Resident leastPreferred() {
+        if (matchedCount == 0) {
+            return null; // pas de resident
+        }
+        Resident leastPreferredResident = matchedResidents[0];
+        int leastPreferredRank = rank(leastPreferredResident.getId());
+        for (int i = 0; i < matchedCount; i++) {
+            Resident residentID = matchedResidents[i]; // convertir en int
+            int rank = rank(residentID.getId());
+            if (rank > leastPreferredRank) { // on veut le rang le plus élevé 
+                leastPreferredRank = rank;
+                leastPreferredResident = residentID;
+            }
+        }
+        return leastPreferredResident;
+    }
 
-	public void addResident(int resident){ // pas faite - ajoute resident a la liste de jumeles / selected pour le programme 
-		
-	
+    public void removeMatchedResident(Resident r) {
+        int index = -1;
+        for (int i = 0; i < matchedCount; i++) {
+            if (matchedResidents[i].getId() == r.getId()) {
+                index = i;
+                break;
+            }
 
-		
+        }
+        if (index == -1) {
+            return;
+        }
 
-	}
+        for (int i = index; i < matchedCount - 1; i++) {
+            matchedResidents[i] = matchedResidents[i + 1];
+        }
+
+        matchedCount--;
+        matchedResidents[matchedCount] = null;
+
+    }
+
+    public Resident addResident(Resident r) { // pas faite - ajoute resident a la liste de jumeles / selected pour le programme 
+
+        // etape 1
+        if (!member(r.getId())) {
+            return r;
+        }
+        //etape 2 
+        if (matchedCount < quota) {
+            matchedResidents[matchedCount] = r;
+            matchedCount++;
+            return null;
+        }
+
+        // etape 3 
+        Resident worst = leastPreferred();
+        int rangR = rank(r.getId());
+        int rangWorst = rank(worst.getId());
+        // Si le candidat est meilleur que le pire, on remplace
+        if (rangR < rangWorst) {
+            removeMatchedResident(worst);
+
+            matchedResidents[matchedCount] = r;
+            matchedCount++;
+
+            return worst;
+        }
+
+        // Sinon le candidat est rejete
+        return r;
+
+    }
 }
--- Resident.java
+++ Resident.java
@@ -4,101 +4,99 @@
 
 // this is the (incomplete) Resident class
 public class Resident {
-	
-	private int residentID;
-	private String firstname;
-	private String lastname;
-	private String[] rol;
-	private String matchedProgram;
-	private String matchedRank;
-	private int nextProgramIndex=0; // index du prochain programme à proposer
-	
-	// constructs a Resident
+
+    private int residentID;
+    private String firstname;
+    private String lastname;
+    private String[] rol;
+    private String matchedProgram;
+    private int matchedRank;
+    private int nextProgramIndex = 0; // index du prochain programme à proposer
+
+    // constructs a Resident
     public Resident(int id, String fname, String lname) {
-	
-		residentID= id;
-		firstname= fname;
-		lastname= lname;
-		matchedProgram= null;
-		rol= new String[0];
 
-	}
+        residentID = id;
+        firstname = fname;
+        lastname = lname;
+        matchedProgram = null;
+        rol = new String[0];
 
-	
+    }
 
     // the rol in order of preference
-	public void setROL(String[] rol) {
-		
-		this.rol= rol;
-	}
+    public void setROL(String[] rol) {
 
-	public String[] getROL(){
-		return rol;
-	}
+        this.rol = rol;
+    }
 
-	public void setId(int Id){
-		this.residentID = Id;
-	}
-	
-	public int getId(){
-		return residentID;
-	}
+    public String[] getROL() {
+        return rol;
+    }
 
-	public void setFirstName(String fname){
-		this.firstname = fname;
-	}
-	
-	public String getFirstName(){
-		return firstname;
-	}
+    public void setId(int Id) {
+        this.residentID = Id;
+    }
 
-	public void setLastName(String lname){
-		this.lastname = lname;
-	}
-	
-	public String getLastName(){
-		return lastname;
-	}
+    public int getId() {
+        return residentID;
+    }
 
-	// string representation
-	public String toString() {
-      
-       return "["+residentID+"]: "+firstname+" "+ lastname+" ("+rol.length+")";	  
-	}
+    public void setFirstName(String fname) {
+        this.firstname = fname;
+    }
 
-	public boolean hasMorePrograms(){ // on veux voir savoir si il y a plus de programmes à proposer
-		return nextProgramIndex < rol.length;
-	}
+    public String getFirstName() {
+        return firstname;
+    }
 
-	public void nextProgram(){ // on incrémente l'index du prochain programme à proposer
-		nextProgramIndex++;
-	}
+    public void setLastName(String lname) {
+        this.lastname = lname;
+    }
 
-	public String getCurrentProgram(){ // on retourne le programme courant à proposer
-		if (nextProgramIndex < rol.length){
-			return rol[nextProgramIndex];
-		}
-		return null;
-	}
+    public String getLastName() {
+        return lastname;
+    }
 
-	public void setMatchedProgram(String program){// on assigne le programme matché
-		this.matchedProgram = program;
-	}
+    // string representation
+    public String toString() {
 
-	public String getMatchedProgram(){ // on retourne le programme matché
-		return matchedProgram;
-	}
+        return "[" + residentID + "]: " + firstname + " " + lastname + " (" + rol.length + ")";
+    }
 
-	public boolean isMatched(){ // on vérifie si le résident est matché
-		return matchedProgram != null;
-	}
+    public boolean hasMorePrograms() { // on veux voir savoir si il y a plus de programmes à proposer
+        return nextProgramIndex < rol.length;
+    }
 
-	public int getRankOfMatchedProgram(){ // on retourne le rang du programme matché
-		for (int i = 0; i < rol.length; i++){
-			if (rol[i].equals(matchedProgram)){
-				return i;
-			}
-		}
-		return -1; // si le programme n'est pas trouvé
-	}
+    public void nextProgram() { // on incrémente l'index du prochain programme à proposer
+        nextProgramIndex++;
+    }
+
+    public String getCurrentProgram() { // on retourne le programme courant à proposer
+        if (nextProgramIndex < rol.length) {
+            return rol[nextProgramIndex];
+        }
+        return null;
+    }
+
+    public void setMatchedProgram(String program) {// on assigne le programme matché
+        this.matchedProgram = program;
+    }
+
+    public String getMatchedProgram() { // on retourne le programme matché
+        return matchedProgram;
+    }
+
+    public boolean isMatched() { // on vérifie si le résident est matché
+        return matchedProgram != null;
+    }
+
+    public int getRankOfMatchedProgram() { // on retourne le rang du programme matché
+        for (int i = 0; i < rol.length; i++) {
+            if (rol[i].equals(matchedProgram)) {
+                return i;
+            }
+        }
+        return -1; // si le programme n'est pas trouvé
+    }
 }
